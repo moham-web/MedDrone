9202a126ec2123af8b5996b1ffd22d991628d4c8
moham-web <119460572+moham-web@users.noreply.github.com>
2025-02-08 14:05:28 +0300

    Delete users directory

--- users/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-
-
-class LoginForm(AuthenticationForm):
-    username = forms.CharField(label="Username", max_length=100)
-    password = forms.CharField(widget=forms.PasswordInput, label="Password")
